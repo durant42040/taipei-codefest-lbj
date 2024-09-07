d17db888c941e254825bd01ff32c7988f05000b3
HubertC1 <bb.chang@gmail.com>
2024-09-08 02:31:12 +0800

    change schema precision from 5 to 7

--- TownPass-Service/backend/src/db/schema.ts
+++ TownPass-Service/backend/src/db/schema.ts
@@ -4,8 +4,8 @@ export const users = pgTable('users', {
     id: varchar('id', {length: 200}).primaryKey(),
     name: varchar('name', {length: 50}),
     age: integer('age'),
-    weight: decimal('weight', {precision: 5, scale: 2}),
-    height: decimal('height', {precision: 5, scale: 2}),
+    weight: decimal('weight', {precision: 7, scale: 2}),
+    height: decimal('height', {precision: 7, scale: 2}),
     gender: varchar('gender', {length: 10}),
 });
 
@@ -14,7 +14,7 @@ export const sessions = pgTable('sessions', {
   userId: varchar('user_id').references(() => users.id),
   sport: varchar('sport', {length: 50}),
   duration: varchar('duration', {length: 8}), // Store duration
-  calories: decimal('calories', {precision: 5}),
+  calories: decimal('calories', {precision: 7}),
   location: varchar('location', {length: 255}),
     // store postgres timestamp
     time: timestamp('time').defaultNow(),
@@ -25,7 +25,7 @@ export const sessions = pgTable('sessions', {
 export const weights = pgTable('weights', {
     id: serial('id').primaryKey(),
     userId: varchar('user_id').references(() => users.id), // Foreign key to users table
-    weight: decimal('weight', {precision: 5, scale: 2}), // User's weight (e.g., 75.50 kg)
+    weight: decimal('weight', {precision: 7, scale: 2}), // User's weight (e.g., 75.50 kg)
     month: integer('month'),
 });
 
@@ -34,10 +34,10 @@ export const foodLog = pgTable('food_log', {
   userId: varchar('user_id').references(() => users.id),
   food: varchar('food', {length: 100}),
   amount: varchar('amount', {length: 100}),
-  calories: decimal('calories', {precision: 5, scale: 2}),
-  protein: decimal('protein', {precision:5, scale:2}),
-  carbo: decimal('carbo', {precision:5, scale:2}),
-  fat: decimal('fat', {precision:5, scale:2}),
+  calories: decimal('calories', {precision: 7, scale: 2}),
+  protein: decimal('protein', {precision:7, scale:2}),
+  carbo: decimal('carbo', {precision:7, scale:2}),
+  fat: decimal('fat', {precision:7, scale:2}),
   time: timestamp('time').defaultNow(),
 });
 
