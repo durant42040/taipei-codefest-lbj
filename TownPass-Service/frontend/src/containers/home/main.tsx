5a825f7bd9ccfa1f1609a09bf31f5b7a3e167344
durant42040 <b11901040@ntu.edu.tw>
2024-09-08 01:10:17 +0800

    add login routing

--- TownPass-Service/frontend/src/containers/home/main.tsx
+++ TownPass-Service/frontend/src/containers/home/main.tsx
@@ -50,12 +50,12 @@ function Home() {
     return "journal"; // default tab
   };
 
-  // useEffect(() => {
-  //   // alert(userData.name);
-  //   if (userData.name !== "" && userData.age === "") {
-  //     navigate("/LoginPage");
-  //   }
-  // }, [navigate, userData]);
+  useEffect(() => {
+    // alert(userData.name);
+    if (userData.name !== "" && userData.age === "") {
+      navigate("/LoginPage");
+    }
+  }, [navigate, userData]);
 
   const handleTabChange = (value: string) => {
     const tab = tabs.find((tab) => tab.value === value);
