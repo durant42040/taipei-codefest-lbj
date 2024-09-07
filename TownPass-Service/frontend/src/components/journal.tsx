647190053879b43edf74e83827be8cecd332c98c
durant42040 <b11901040@ntu.edu.tw>
2024-09-07 12:33:33 +0800

    add jornal

---
+++ TownPass-Service/frontend/src/components/journal.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Journal = () => {
+  return (
+    <div>
+      <p>This is a placeholder for the journal component.</p>
+    </div>
+  );
+};
+
+export default Journal;
