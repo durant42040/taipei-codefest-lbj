ef171063e1dd8055618b410275d1f46a9e54aff2
HubertC1 <bb.chang@gmail.com>
2024-09-07 15:12:36 +0800

    add activity log/ add home route

--- TownPass-Service/frontend/src/App.tsx
+++ TownPass-Service/frontend/src/App.tsx
@@ -1,6 +1,8 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import WeightTrackingPage from "./components/weight"; // Import your weight page
-import Home from "@/containers/journal/main"; // Import the main.tsx (journal stuff) as Home component
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import WeightTrackingPage from "./components/weight" // Import your weight page
+import Home from "@/containers/home/main" // Import the main.tsx (journal stuff) as Home component
+import ExerciseDetails from "./containers/exerciseLog/main"
 
 
 function App() {
@@ -9,10 +11,11 @@ function App() {
       <div>
         {/* React Router Routes */}
         <Routes>
-          <Route path="/" element={<Home />} />{" "}
-          {/* Home page (Journal stuff from main.tsx) */}
-          <Route path="/weight" element={<WeightTrackingPage />} />{" "}
-          {/* Routing to weight.tsx */}
+
+          <Route path="/" element={<Home />} /> {/* Home page (Journal stuff from main.tsx) */}
+          <Route path="/weight" element={<WeightTrackingPage />} /> {/* Routing to weight.tsx */}
+          <Route path="/exerciseDetails" element={<ExerciseDetails />} />
+
           {/* Add other routes as needed */}
         </Routes>
       </div>
