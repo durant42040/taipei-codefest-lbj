2db3f0a4f8f2981ba96ea330500d40fc67c108ee
durant42040 <b11901040@ntu.edu.tw>
2024-09-08 04:12:41 +0800

    fix ugly food

--- TownPass-Service/frontend/src/components/ui/foodCard.tsx
+++ TownPass-Service/frontend/src/components/ui/foodCard.tsx
@@ -31,28 +31,32 @@ export function FoodActivityCard({
       <CardContent className="p-4">
         <div className="flex flex-row items-center gap-2.5 justify-between">
           <div className="flex items-center space-x-2">
-            <Pizza className="w-4 h-4 text-muted-foreground" />
+            <Pizza className="w-4 h-4 text-muted-foreground"/>
             <span className="text-sm font-medium">{food}</span>
           </div>
           <div className="flex items-center space-x-2">
-            <Weight className="w-4 h-4 text-muted-foreground" />
+            <Weight className="w-4 h-4 text-muted-foreground"/>
             <span className="text-sm font-medium">{amount}</span>
           </div>
           <div className="flex items-center space-x-1 min-w-20">
-            <Flame className="w-4 h-4 text-muted-foreground" />
-            <span className="text-sm font-medium">{calories} cal</span>
+            <Flame className="w-4 h-4 text-muted-foreground"/>
+            <span className="text-sm font-medium">{calories} kcal</span>
           </div>
-          <div className="flex items-center space-x-4 min-w-64">
+        </div>
+        <div className="flex space-x-4 min-w-64">
             <span
-              title="紅色: 蛋白質 (克) | 黃色: 脂肪 (克) | 綠色: 碳水化合物 (克)"
-              className="cursor-help"
+                title="紅色: 蛋白質 (克) | 黃色: 脂肪 (克) | 綠色: 碳水化合物 (克)"
+                className="flex items-center space-x-1 mt-2"
             >
               <span className="text-red-500 text-sm">{protein}</span> /{" "}
               <span className="text-yellow-500 text-sm">{fat}</span> /{" "}
               <span className="text-green-500 text-sm">{carbo}</span>
-              <Info className="inline-block ml-1.5 w-4 h-4" />
+              <Info className="inline-block ml-1.5 w-4 h-4" onClick={
+                (e) => {
+                  e.stopPropagation();
+                  alert("紅色: 蛋白質 (克) | 黃色: 脂肪 (克) | 綠色: 碳水化合物 (克)");
+              }}/>
             </span>
-          </div>
         </div>
       </CardContent>
     </Card>
